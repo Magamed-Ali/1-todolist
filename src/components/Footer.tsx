c3a6d8141c0abbff24d97dafa4d5d549e38921b2
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2023-02-15 00:51:04 +0300

    add stayle

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Footer() {
+    return (
+        <div className="footer">
+            Footer
+            <div className="footer-line"></div>
+        </div>
+    );
+}
