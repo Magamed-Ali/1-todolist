66fa24a26bb53a268884261bee411e931257c4f5
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2023-02-18 22:51:18 +0300

    add material-ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export function Footer() {
+    var newDate = new Date().toLocaleDateString();
+
+
     return (
         <div className="footer">
-            Footer
+            <div>© {newDate} Copyright Text</div>
+
+            <div>
+                Link: <a href="https://github.com/Magamed-Ali/1-todolist">GitHab project</a>
+            </div>
+
             <div className="footer-line"></div>
         </div>
     );
