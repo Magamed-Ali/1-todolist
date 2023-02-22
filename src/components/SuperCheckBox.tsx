7924fef6e86102ef12dad3cf8e6edc0a0fcde3e0
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2023-02-22 13:38:49 +0300

    lesson №8

--- src/components/SuperCheckBox.tsx
+++ src/components/SuperCheckBox.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
+import Checkbox from "@mui/material/Checkbox";
 
-function SuperCheckBox() {
+type TypeCheck = {
+    task: boolean
+    callBack: (check: boolean) => void
+}
+function SuperCheckBox(props: TypeCheck) {
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        props.callBack(e.currentTarget.checked)
+    }
     return (
-        <div></div>
+        <Checkbox
+            checked={props.task}
+            defaultChecked
+            onChange={onChangeHandler}
+            style={{maxWidth: "35px", minWidth: "35px", height: "35px"}}
+        />
     );
 }
 
